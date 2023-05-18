2cfd7be166b620a585fb1da1f02bae35dfa69424
eshdownbad <eshdownbad@users.noreply.github.com>
2023-05-18 21:05:12 +0530

    fixed issue with not dowloading all images

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use core::panic;
 use reqwest::Url;
-use std::{env, error, str::FromStr, path::PathBuf};
-use tokio::{fs , io::AsyncWriteExt};
+use std::{env, error, path::PathBuf, str::FromStr};
+use tokio::{fs, io::AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let url = Url::from_str(args.get(1).expect("url not found")).expect("cannot parse url");
+
     println!("fetching image count...");
     let count: u16;
     match get_count(url.clone()).await {
@@ -14,61 +15,73 @@ async fn main() {
         Err(e) => panic!("error: {}", e),
     }
     println!("posts count: {count}");
+    let mut download_count: u16 = 0;
+    while count > download_count {
+        let mut range_start = 1;
+        let mut range_end = count;
+        if download_count > 0 {
+            range_start = download_count + 1;
+            println!("{count} , {download_count}");
+            range_end = (count - download_count) + count;
+        }
 
-    println!("fetching image urls...");
-    let vec_size: usize = count.into();
-    let mut url_handles = Vec::with_capacity(vec_size);
-    for num in 1..count + 1 {
-        let url = url.clone().join(&num.to_string()).unwrap();
-       url_handles.push(tokio::spawn(get_image_url(url.clone())));
+        let mut image_urls: Vec<String> = Vec::new();
+
+        println!("fetching image urls...");
+
+        let mut url_handles = Vec::new();
+
+        for num in range_start..range_end + 1 {
+            let url = url.clone().join(&num.to_string()).unwrap();
+            url_handles.push(tokio::spawn(get_image_url(url.clone())));
+        }
+
+        for handle in url_handles {
+            match handle.await.unwrap() {
+                Ok(val) => {
+                    download_count += 1;
+                    image_urls.push(val)
+                }
+                Err(err) => {
+                    if matches!(err, GetImageUrlErrors::PostDoesntExist(_)) {
+                        println!("image #{:?} doesnt exist", err)
+                    }
+                }
+            };
+        }
+        //TODO create an arg for this
+        let base_path = PathBuf::from("./tmp");
+        let mut download_handlers = Vec::new();
+        for url in image_urls {
+            download_handlers.push(downlaod_image(url, base_path.clone()))
+        }
+        for handle in download_handlers {
+            handle.await.unwrap()
+        }
     }
-    let mut not_found_posts_count = 0;
-    let mut image_urls: Vec<String> = Vec::with_capacity(vec_size);
-    for handle in url_handles {
-        match handle.await.unwrap() {
-         Ok(val) => image_urls.push(val),
-         Err(err) => { if matches!(err ,GetImageUrlErrors::PostDoesntExist ){
-            not_found_posts_count += 1;
-         } }
-        };
-    }
-    //adds getters for images have the id bigger than count. this happens cause some images may be deleted.
-    
-    println!("image urls fetched");
-    //add a way for cmd line args here
-    let base_path = PathBuf::from("./tmp");
-    println!("image urls: {:?}" , image_urls);
-    let mut download_handlers = Vec::with_capacity(vec_size);
-    println!("starting downloads...");
-    for url in image_urls {
-        download_handlers.push(downlaod_image(url, base_path.clone()))
-    }
-    for handle in download_handlers {
-        handle.await.unwrap()
-    }
-    println!("download completed!");
-    println!("didnt find {not_found_posts_count} images");
+    println!("downloaded {download_count} images");
 }
 
-
-async fn downlaod_image(url: String , base_path: PathBuf) -> Result<() , Box<dyn error::Error + Sync + Send>> {
+async fn downlaod_image(
+    url: String,
+    base_path: PathBuf,
+) -> Result<(), Box<dyn error::Error + Sync + Send>> {
     let filename = url.split("/").last().unwrap();
-    let filepath =base_path.join(filename);
-   let mut file = fs::File::create(filepath).await?;
-   let mut img_data= reqwest::get(url.clone()).await?.bytes().await?;
-   file.write_all_buf(&mut img_data).await?;
-   println!("saved file: {filename}");
+    let filepath = base_path.join(filename);
+    let mut file = fs::File::create(filepath).await?;
+    let mut img_data = reqwest::get(url.clone()).await?.bytes().await?;
+    file.write_all_buf(&mut img_data).await?;
+    println!("saved file: {filename}");
     Ok(())
 }
 
 #[derive(Debug, Clone)]
 struct PostDoesntExist;
 
-
 #[derive(Debug)]
 enum GetImageUrlErrors {
     RequestError(reqwest::Error),
-    PostDoesntExist
+    PostDoesntExist(u16),
 }
 
 impl From<reqwest::Error> for GetImageUrlErrors {
@@ -80,13 +93,13 @@ impl From<reqwest::Error> for GetImageUrlErrors {
 async fn get_image_url(url: Url) -> Result<String, GetImageUrlErrors> {
     let html = reqwest::get(url.clone()).await?.text().await?;
     let dom = tl::parse(&html, tl::ParserOptions::default()).unwrap();
-    let mut qs_iter = dom
-        .query_selector("a.uk-align-center").unwrap();
-        let handle = qs_iter.next();
-        //sometimes images are deleted from the site so this exists for that
-if handle == None {
-   return  Err(GetImageUrlErrors::PostDoesntExist);
-}
+    let mut qs_iter = dom.query_selector("a.uk-align-center").unwrap();
+    let handle = qs_iter.next();
+    //sometimes images are deleted from the site so this exists for that
+    if handle == None {
+        let post_num: u16 = url.to_string().split("/").last().unwrap().parse().unwrap();
+        return Err(GetImageUrlErrors::PostDoesntExist(post_num));
+    }
     let node = handle.unwrap().get(dom.parser()).unwrap();
     let link = node
         .as_tag()
@@ -94,7 +107,9 @@ if handle == None {
         .attributes()
         .get("href")
         .unwrap()
-        .unwrap().try_as_utf8_str().unwrap();
+        .unwrap()
+        .try_as_utf8_str()
+        .unwrap();
 
     Ok(String::from(link))
 }
