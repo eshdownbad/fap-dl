9fbedff843ceeb20c0b049c5aedf00e2ebc39b42
eshdownbad <eshdownbad@users.noreply.github.com>
2023-05-20 21:58:47 +0530

    improved algorithm for finding num of imgs

--- readme.md
+++ readme.md
@@ -2,5 +2,16 @@
 
 Command-line program to download videos from fapello.com
 
+## usage instructions
+
+1. clone the repo 
+2. run `cargo build --release` to use or just `use cargo run` (cargo run is usually slower)
+3. use cmd as follows: 
+    - with cargo run:
+        `cargo run -- [fapello url]  [path to save dir]`
+    - or after cargo build use as:
+        `./target/release/fap-dl [fapello url] [path to save dir] `
+
+
 ## Disclaimer
 i'm new to using rust so this code base has a lot of bad code that ill fix eventually. 
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,53 @@
 use core::panic;
 use reqwest::Url;
-use std::{env, error, path::{PathBuf}, str::FromStr, };
+use std::{env, error, path::PathBuf, str::FromStr};
 use tokio::{fs, io::AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    let save_location_arg = args.get(2).expect("2nd arg needs to be a path to save folder");
+    let save_location_arg = args
+        .get(2)
+        .expect("2nd arg needs to be a path to save folder");
     let save_location = PathBuf::from(save_location_arg);
     if !save_location.is_dir() {
         println!("save folder path doesnt exist attempting to create...");
         match fs::create_dir_all(&save_location).await {
-            Ok(_) => { println!("save path folder created")},
-            Err(err) =>  {
-                panic!("{}" , err)
+            Ok(_) => {
+                println!("save path folder created")
             }
-           };
-    } 
-
+            Err(err) => {
+                panic!("{}", err)
+            }
+        };
+    }
+    //the first arg is the fapello url so yeah
     let url = Url::from_str(args.get(1).expect("url not found")).expect("cannot parse url");
 
-    println!("fetching image count...");
-    let count: u16;
-    match get_count(url.clone()).await {
-        Ok(val) => count = val.expect("post count could not be retrieved"),
-        Err(e) => panic!("error: {}", e),
-    }
-    println!("posts count: {count}");
-    let mut download_count: u16 = 0;
-    while count > download_count {
-        let mut range_start = 1;
-        let mut range_end = count;
-        if download_count > 0 {
-            range_start = download_count + 1;
-            println!("{count} , {download_count}");
-            range_end = (count - download_count) + count;
-        }
+    //this is the last id on fapello aka the first post in the grid.
+    //there cant be ids bigger than this so yeah going from 1 to this good enough
+    let largest_id = get_latest_id(url.clone()).await.unwrap();
 
-        let mut image_urls: Vec<String> = Vec::new();
+    //hate needing to do conversions like this one but eh
+    let vec_size = largest_id.try_into().unwrap();
 
-        println!("fetching image urls...");
+    //contains all the urls for images that are going to be saved to disk
+    let mut image_urls = Vec::with_capacity(vec_size);
 
-        let mut url_handles = Vec::new();
+    //put this in a diff block cause wont rlly need url_handlers once this block is done
+    {
+        let mut url_handles = Vec::with_capacity(vec_size);
 
-        for num in range_start..range_end + 1 {
-            let url = url.clone().join(&num.to_string()).unwrap();
+        //creates all image post urls from 1 to the last id
+        for i in 1..(largest_id + 1) {
+            let url = url.clone().join(&i.to_string()).unwrap();
             url_handles.push(tokio::spawn(get_image_url(url.clone())));
         }
 
+        //welp time to await i should prolly add fetching images in the same function but fuck it we ball :)
         for handle in url_handles {
             match handle.await.unwrap() {
-                Ok(val) => {
-                    download_count += 1;
-                    image_urls.push(val)
-                }
+                Ok(val) => image_urls.push(val),
                 Err(err) => {
                     if matches!(err, GetImageUrlErrors::PostDoesntExist(_)) {
                         println!("image #{:?} doesnt exist", err)
@@ -61,20 +55,24 @@ async fn main() {
                 }
             };
         }
-        //TODO create an arg for this
-        let base_path = save_location.clone();
-        let mut download_handlers = Vec::new();
-        for url in image_urls {
-            println!("{url}");
-            download_handlers.push(downlaod_image(url, base_path.clone()))
-        }
-        for handle in download_handlers {
-            handle.await.unwrap()
-        }
     }
-    println!("downloaded {count} images");
+    //welp time to download yay
+    let mut download_handlers = Vec::new();
+    for url in image_urls {
+        download_handlers.push(downlaod_image(url, save_location.clone()))
+    }
+    for handle in download_handlers {
+        handle.await.unwrap()
+    }
+
+    println!("download complete");
 }
 
+
+/**
+ * basic function just creates the file and copies the bytes from reqwest
+ * thankfully this wasnt mind numbing to figure out (lie) :)
+ */
 async fn downlaod_image(
     url: String,
     base_path: PathBuf,
@@ -88,8 +86,6 @@ async fn downlaod_image(
     Ok(())
 }
 
-#[derive(Debug, Clone)]
-struct PostDoesntExist;
 
 #[derive(Debug)]
 enum GetImageUrlErrors {
@@ -103,11 +99,12 @@ impl From<reqwest::Error> for GetImageUrlErrors {
     }
 }
 
+/** this function parses the post html and gets the image url */
 async fn get_image_url(url: Url) -> Result<String, GetImageUrlErrors> {
     let res = reqwest::get(url.clone()).await?;
     if res.url().to_string() == "https://fapello.com/" {
         let post_num: u16 = url.to_string().split("/").last().unwrap().parse().unwrap();
-        return Err(GetImageUrlErrors::PostDoesntExist(post_num)); 
+        return Err(GetImageUrlErrors::PostDoesntExist(post_num));
     }
     let html = res.text().await?;
     let dom = tl::parse(&html, tl::ParserOptions::default()).unwrap();
@@ -127,23 +124,57 @@ async fn get_image_url(url: Url) -> Result<String, GetImageUrlErrors> {
     Ok(String::from(link))
 }
 
-async fn get_count(url: Url) -> Result<Option<u16>, Box<dyn error::Error>> {
+/**returns the id of the latest image uploaded on fapello
+TODO use struct for error */
+async fn get_latest_id(url: Url) -> Result<u64, Box<dyn error::Error>> {
     let res = reqwest::get(url.clone()).await?;
-    //if there is a redirect to main page that means the page was not found
+    //if there is a redirect to home page it means the requested page was not found
+    // why is this site like this why couldnt they just give us a 404 ;-;
     if res.url().to_string() == "https://fapello.com/" {
-        return Ok(None);
+        //TODO create error type for this
+        panic!("{url} does not exist :(")
     }
-    //selcts and extracts the post count  from the html
-    //there is a def a better way to do this
     let html = res.text().await?;
+    /* idk why but i cant just use query selector the same way i can for js
+    prolly smth to do with settings but ill figure that out later */
     let dom = tl::parse(&html, tl::ParserOptions::default()).unwrap();
-    let  handle = dom.query_selector("div.divide-gray-300.divide-transparent.divide-x.grid.grid-cols-2.lg:text-left.lg:text-lg.mt-3.text-center.w-full.dark:text-gray-100")
-    .and_then(|mut iter| iter.next()).unwrap();
-
-    let node = handle.get(dom.parser()).unwrap().inner_text(dom.parser());
-    let count = node.split(" ").nth(1);
-    if count == None {
-        return Ok(None);
-    }
-    return Ok(Some(count.unwrap().parse::<u16>()?));
+    // this is a hell hole and a tumour to js select #content > div > a
+    //there is def a better way to do this idk how tho
+    //there are too many unwraps and magic numbers needed cause there are random whitespaces bruh
+    //prolly strip the whitespaces before parsing but this works too
+    let node = dom
+        .query_selector("#content")
+        .and_then(|mut iter| iter.next())
+        .unwrap()
+        .get(dom.parser())
+        .unwrap()
+        .children()
+        .unwrap()
+        .all(dom.parser())
+        .get(1)
+        .unwrap()
+        .as_tag()
+        .unwrap()
+        .children()
+        .all(dom.parser())
+        .get(1)
+        .unwrap();
+    //after the hell hole its simple enough to js extract the link from the href
+    let link = node
+        .as_tag()
+        .unwrap()
+        .attributes()
+        .get("href")
+        .unwrap()
+        .unwrap()
+        .try_as_utf8_str()
+        .unwrap();
+    //do i rlly need to create a vec to get the 2nd last element (its the id) here? prolly no
+    //wondering why id is 2nd last and not last? 
+    //cause there is a slash in the end so last element is an empty string 
+    //but fuck it we ball
+    let link_parts = link.split("/").collect::<Vec<_>>();
+    println!("{:?}", link_parts.get(link_parts.len() - 2));
+    let last_id: u64 = link_parts.get(link_parts.len() - 2).unwrap().parse()?;
+    return Ok(last_id);
 }
